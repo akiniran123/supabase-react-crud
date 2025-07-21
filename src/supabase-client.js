84b8df38612ec57385710a45170975d98c5fda34
akiniran123 <69475298+akiniran123@users.noreply.github.com>
2025-07-21 17:43:32 +0700

    ducanh
    ducanh

--- src/supabase-client.js
+++ src/supabase-client.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-
+// Initialize Supabase client with your credentials
+const supabaseUrl = "https://kzfmunxbdfdfxumerulj.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6Zm11bnhiZGZkZnh1bWVydWxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwODI3ODAsImV4cCI6MjA2ODY1ODc4MH0.loX0aIS6RsXCO6_WlmmkvcHXgZH0WPlLR3exRAFXFA0";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
